94fac8e12611886553c506fd5ccf00c94f0dd333
GUL-Bao <BaoPham92@yahoo.com>
2019-10-10 11:20:14 -0400

    pkgs added, store setup, state refactor, inital commit

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -5,7 +5,7 @@ const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.store.length ? (
+      {props.store && props.store.length ? (
         <ol type="1">
           {props.store.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
